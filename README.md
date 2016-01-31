b6b9bd34fd858da4e77f2c45147716b95eec359b
Jiaxiang Zheng <jiaxiang.zheng135@gmail.com>
2016-01-31 12:35:50 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+a face++ sdk for node version.
+
+## How to use?
+
+```
+let FacePP = require('./apis');
+
+var facePP = new FacePP({
+  SERVER_URL: 'http://apicn.faceplusplus.com',
+  API_KEY: 'YOUR_API_KEY',
+  API_SECRET: 'YOUR_API_SECRET'
+});
+
+facePP.detection.detect({
+	url: 'http://img.sucai.redocn.com/attachments/images/201204/20120417/Redocn_2012041310435227.jpg',
+	attribute: 'glass,pose,landmark,gender,age,race,smiling'
+}).then((body) => {
+	console.log(body);
+}, console.log);
+```
---
+++ src/apis.js
@@ -0,0 +1,113 @@
+'use strict';
+let request = require('request');
+let querystring = require('querystring');
+
+const api_tree = {
+  detection: {
+    detect: '',
+    landmark: '',
+  },
+  train: {
+    verify: '',
+    search: '',
+    identity: ''
+  },
+  recognition: {
+    compare: '',
+    verify: '',
+    search: '',
+    identify: ''
+  },
+  grouping: {
+    grouping: ''
+  },
+  person: {
+    'delete': '',
+    add_face: '',
+    remove_face: '',
+    set_info: '',
+    get_info: ''
+  },
+  faceset: {
+    'create': '',
+    'delete': '',
+    add_face: '',
+    remove_face: '',
+    set_info: '',
+    get_info: ''
+  },
+  group: {
+    create: '',
+    delete: '',
+    add_person: '',
+    remove_person: '',
+    get_info: '',
+  },
+  info: {
+    get_image: '',
+    get_face: '',
+    get_person_list: '',
+    get_faceset_list: '',
+    get_group_list: '',
+    get_session: '',
+    get_app: ''
+  }
+}
+
+class FacePP {
+	constructor(config) {
+		this.SERVER_URL = config.SERVER_URL;
+		this.API_KEY = config.API_KEY;
+    this.API_SECRET = config.API_SECRET;
+
+    this.init();
+	}
+
+  init() {
+    this._flatten(api_tree, this, []);
+  }
+
+  _flatten(obj, res, level_arr) {
+    for (let key in obj) {
+      if (typeof obj[key] === 'string' || !obj[key]) {
+        res[key] = this._get_api('/' + level_arr.concat([key]).join('/'));
+      } else {
+        res[key] = this._flatten(obj[key], {}, level_arr.concat([key]));
+      }
+    }
+    return res;
+  }
+
+  _get_api(url) {
+    return (options) => {
+      return new Promise((resolve, reject) => {
+        let queryString = querystring.stringify(Object.assign({
+          api_secret: this.API_SECRET,
+          api_key: this.API_KEY,
+        }, options));
+        request({
+          baseUrl: this.SERVER_URL,
+          method: 'GET',
+          url: url + '?' + queryString
+        }, (err, response, body) => {
+          if (err) {
+            reject(err);
+            return;
+          }
+          let statusCode = response.statusCode;
+
+          if (typeof body === 'string') {
+            body = JSON.parse(body);
+          }
+          if (statusCode !== 200) {
+            reject(new Error(body.error));
+            return;
+          }
+          resolve(body);
+        });
+      });
+    }
+  }
+}
+
+module.exports = FacePP;
